07c39762c8090025fc73e3b91054dadb9186a0e0
AdreXavier <165933295+AdreXavier@users.noreply.github.com>
2026-03-02 10:25:27 -0300

    Refactor middleware to protect all routes

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,13 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-// Definimos que a página inicial (/) e /index são públicas
-const isPublicRoute = createRouteMatcher(['/', '/index'])
-
-export default clerkMiddleware(async (auth, request) => {
-  if (!isPublicRoute(request)) {
-    await auth.protect()
-  }
+export default clerkMiddleware(async (auth) => {
+  // Isso obriga o login em todas as rotas que o matcher encontrar
+  await auth.protect()
 })
 
 export const config = {
   matcher: [
-    // Pula arquivos internos do Next.js e arquivos estáticos (imagens, etc)
+    // Pula arquivos internos do Next.js e arquivos estáticos
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Sempre executa para rotas de API
     '/(api|trpc)(.*)',
