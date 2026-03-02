4fbc536b325a29db9f0d47309d6aae56e9e8e9b3
AdreXavier <165933295+AdreXavier@users.noreply.github.com>
2026-03-02 10:47:14 -0300

    Refactor middleware configuration and comments

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,13 @@
 import { clerkMiddleware } from '@clerk/nextjs/server'
 
 export default clerkMiddleware(async (auth) => {
-  // Isso obriga o login em todas as rotas que o matcher encontrar
+  // Tranca absolutamente tudo
   await auth.protect()
 })
 
 export const config = {
   matcher: [
-    // Pula arquivos internos do Next.js e arquivos estáticos
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Sempre executa para rotas de API
     '/(api|trpc)(.*)',
   ],
 }
