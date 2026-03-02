78c58fde41966b26bd4c1af372be127c83eff5b7
AdreXavier <165933295+AdreXavier@users.noreply.github.com>
2026-03-02 13:21:18 +0000

    ajuste final login

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,19 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export default authMiddleware({
-  // O que for público (Home)
-  publicRoutes: ["/", "/index"],
-});
+// Definimos que a página inicial (/) e /index são públicas
+const isPublicRoute = createRouteMatcher(['/', '/index'])
+
+export default clerkMiddleware(async (auth, request) => {
+  if (!isPublicRoute(request)) {
+    await auth.protect()
+  }
+})
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+  matcher: [
+    // Pula arquivos internos do Next.js e arquivos estáticos (imagens, etc)
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Sempre executa para rotas de API
+    '/(api|trpc)(.*)',
+  ],
+}
