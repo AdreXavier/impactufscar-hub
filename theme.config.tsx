8f39889d6d777aadc8dc747729b2deed40515082
AdreXavier <165933295+AdreXavier@users.noreply.github.com>
2026-03-02 08:32:42 -0300

    Update theme configuration with new links and icon

--- theme.config.tsx
+++ theme.config.tsx
@@ -8,10 +8,17 @@ const config: DocsThemeConfig = {
   },
   chat: {
     link: 'https://impactufscar.dev',
+    icon: (
+      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M21.2 17.6c0 1.1-.9 2-2 2h-14.4c-1.1 0-2-.9-2-2V6.4c0-1.1.9-2 2-2h14.4c1.1 0 2 .9 2 2v11.2z" fill="#0058CC"/>
+        <path d="M12 16a4 4 0 100-8 4 4 0 000 8z" fill="#fff"/>
+        <path d="M12 14a2 2 0 100-4 2 2 0 000 4z" fill="#0058CC"/>
+      </svg>
+    )
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/AdreXavier/impactufscar-hub',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'ImpactUFSCar 2026',
   },
 }
 
